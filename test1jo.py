0358668e4ad69406395363eb98020b3a87588142
Ahmed525-12 <ahmedsayed5250057@gmail.com>
2021-11-02 20:19:20 +0300

    first commit

---
+++ test1jo.py
@@ -0,0 +1,79 @@
+# from turtle import *
+
+# for i in range(50):
+#     forward(50)
+#     right(90)
+#     forward(50)
+#     left(90)
+#     forward(50)
+
+# there is use looping with import packdes
+
+# for column in range(1,13): 
+#     for row in range(1,13): 
+#         print(column*row,end="   ")
+#     print( )    
+
+
+# sum=0
+# for i in range(5):
+#     sum=sum+i
+# print(sum)    
+
+# lis=[4,5,6,[4,5,6]]
+
+# studntSchool=  [    {"name":"ahmed","age":19},{"name":"yousef","age":15}  ]
+
+# for nameStudent in range(len(studntSchool)):
+#     print(studntSchool[nameStudent],end='  ')
+
+# x=input("enter your first number: ")
+
+# a=int(x)
+ 
+# y=input("enter your second number: ")
+ 
+# b=int(y)
+
+# z=a+b
+
+# print(z)
+
+
+# x=5
+ 
+# if x>7:
+#     print("x bigger than 4")
+# elif x==5 : 
+#     print ("try again") 
+
+
+    # switch(x){
+    #     case 5: print("heloo")
+    # }
+
+# x=0
+
+# while x<=11 :
+#     z=input("enter your first number: ")
+
+#     a=int(z)
+ 
+#     y=input("enter your second number: ")
+ 
+#     b=int(y)
+
+#     x=a+b
+
+#     print("your result is ",x)
+
+def xws(r,u):
+    x=r
+    b=u
+    z=x+b
+    return(z)
+ 
+x=xws(5,5)
+print(x) 
+
+
